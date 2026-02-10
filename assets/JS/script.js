e0a003d3b68aeb8bbdfb9bd22b38c7cb82c0f7b5
Aritra <aritra.03@proton.me>
2026-02-10 19:48:22 +0530

    Fixed bug where it alerts data fetched even if no matching fields are found

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -27,7 +27,7 @@ const searchCompany = async function () {
         const json = await res.json();
 
         if (typeof json.data !== "object") {
-            alert(json.message);
+            alert(json.data);
             return;
         }
 
